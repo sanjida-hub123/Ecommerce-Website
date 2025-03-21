e9b72367ea549454336080206e38d35cd6c85f2d
Sanjida Tahmin Lupa <your-sanjidalupa90gmail.com>
2025-03-21 20:39:52 +0600

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// home section start
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -13,3 +14,24 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+// home section end
+
+// sproduct section start
+var MainImg = document.getElementById("Mainimg");
+var smallimg = document.getElementsByClassName("small-img")
+
+
+smallimg[0].onclick = function () {
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    MainImg.src = smallimg[3].src;
+}
+// sproduct section end
+
